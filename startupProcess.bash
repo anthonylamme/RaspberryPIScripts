9e28ef4cf8a42fc5b6bac9e8b9290d8ae44b82ce
anthonylamme <anthonylamme@gmail.com>
2017-07-26 10:42:34 -0500

    Update startupProcess.bash

--- startupProcess.bash
+++ startupProcess.bash
@@ -12,18 +12,30 @@ echo "Hi, $USER! starting UPGRADE"
 sudo apt-get update
 
 echo "PROGRAMMING"
+#text editor GUI
 sudo apt-get -y install leafpad
-sudo apt-get -y install vim
+#text editor cmd line
+sudo apt-get -y install vim 
 sudo apt-get -y install espeak
+#program arduinos
 sudo apt-get -y install arduino
-sudo apt-get -y install ntpdate
-sudo apt-get -y install nodejs
+sudo apt-get install python
+sudo apt-get install python-serial
+#time update
+sudo apt-get -y install ntpdate 
+#possible use later for cluster monitoring
+sudo apt-get -y install nodejs 
+#Server side programs
 sudo apt-get -y install apache2 apache2-doc apache2-utils
 sudo apt-get -y install libapache2-mod-php5 php5 php-pear php5-xcache
 sudo apt-get -y install php5-mysql
 sudo apt-get install mysql-server mysql-client
+#tool for ssh
 sudo apt-get -y install screen -y
+#slack interface
 sudo pip install slackclient
+#cmdline tool
+sudo apt-get install moreutils
 
 echo "Hi, $USER! starting folder"
 mkdir /home/pi/Scripts
@@ -32,6 +44,7 @@ cd /home/pi/Scripts
 git clone https://github.com/anthonylamme//RaspberryPIScripts.git
 git clone https://github.com/anthonylamme/Pick2Light.git
 git clone https://github.com/anthonylamme/RoboticArmCode.git
+wget https://raw.githubusercontent.com/tbird20d/grabserial/master/grabserial grabserial
 
 echo "Finished"
 sudo reboot
