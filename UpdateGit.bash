dbeb7e8e5af38a1f6b69eacfd9cde4b26ae5d1af
anthonylamme <anthonylamme@gmail.com>
2017-07-31 08:26:53 -0500

    Update UpdateGit.bash

--- UpdateGit.bash
+++ UpdateGit.bash
@@ -1,15 +1,17 @@
+#This Script will update the RPi every time it turns on or is Commanded to by Slack
+
 #!/bin/bash
 newHome=/home/pi/Scripts
 
 echo "Updating Pi"
 
-sudo apt-get update
+sudo apt-get update #update OS
 
-cd /home/pi/Scripts/Pick2Light
-git pull
-cd /home/pi/Scripts/RaspberryPIScripts
-git pull
-cd /home/pi/Scripts/RoboticArmCode
-git pull
+cd /home/pi/Scripts/Pick2Light #change to Pick2Light folder
+git pull #pulls recent version from gitHub
+cd /home/pi/Scripts/RaspberryPIScripts #change to Rasberry PI Script folder
+git pull #pulls recent version from gitHub
+cd /home/pi/Scripts/RoboticArmCode #change to Robotic Arm Code folder
+git pull #pulls recent version from gitHub
 
 echo "finished"
