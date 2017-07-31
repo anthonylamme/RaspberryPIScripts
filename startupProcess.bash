2b0adcb93af787c0fdaae7c061cdddb35f91cfbc
anthonylamme <anthonylamme@gmail.com>
2017-07-31 08:35:25 -0500

    Update startupProcess.bash

--- startupProcess.bash
+++ startupProcess.bash
@@ -1,3 +1,4 @@
+#This Script is designed to be used to setup the RPi so that all of them have the same starting point
 #!/bin/bash
 newHome=/home/pi/scripts
 echo "Starting Script"
@@ -7,7 +8,7 @@ echo "Hi, $USER! starting network"
 
 # install programs
 echo "Hi, $USER! starting UPGRADE"
-sudo apt-get update
+sudo apt-get update #updates OS
 
 echo "PROGRAMMING"
 #text editor GUI
@@ -17,12 +18,12 @@ sudo apt-get -y install vim
 sudo apt-get -y install espeak
 #program arduinos
 sudo apt-get -y install arduino
-sudo apt-get install python
-sudo apt-get install python-serial
+sudo apt-get install python #updates python compiler
+sudo apt-get install python-serial #allows for serial monitoring
 #time update
-sudo apt-get -y install ntpdate 
+sudo apt-get -y install ntpdate #retrieves time and date informaton from internet
 #possible use later for cluster monitoring
-sudo apt-get -y install nodejs 
+sudo apt-get -y install nodejs
 #Server side programs
 sudo apt-get -y install apache2 apache2-doc apache2-utils
 sudo apt-get -y install libapache2-mod-php5 php5 php-pear php5-xcache
@@ -31,26 +32,26 @@ sudo apt-get install mysql-server mysql-client
 #tool for ssh
 sudo apt-get -y install screen -y
 #slack interface
-sudo pip install slackclient
+sudo pip install slackclient 
 #cmdline tool
-sudo apt-get install moreutils
-sudo apt-get install zip
+sudo apt-get install moreutils  #allows for commands that help with zip files and other
+sudo apt-get install zip 
 
-sudo apt-get install i2c-tools
-sudo apt-get install python-smbus
+sudo apt-get install i2c-tools # enables i2c check tools
+sudo apt-get install python-smbus #turns on the python bus for i2c
 
-sudo apt-get install python-dev python-rpi.gpio
+sudo apt-get install python-dev python-rpi.gpio #library used for GPIO controll
 
 echo "Hi, $USER! starting folder"
-mkdir /home/pi/Scripts
+mkdir /home/pi/Scripts #makes central folder for repositories
 cd /home/pi/Scripts
 
-git clone https://github.com/anthonylamme/RaspberryPIScripts.git
-git clone https://github.com/anthonylamme/Pick2Light.git
-git clone https://github.com/anthonylamme/RoboticArmCode.git
-wget https://raw.githubusercontent.com/tbird20d/grabserial/master/grabserial grabserial
+git clone https://github.com/anthonylamme/RaspberryPIScripts.git #general use scripts containing startup,update and git update
+git clone https://github.com/anthonylamme/Pick2Light.git #Pick2Light project programs
+git clone https://github.com/anthonylamme/RoboticArmCode.git #robotic arm code 
+wget https://raw.githubusercontent.com/tbird20d/grabserial/master/grabserial grabserial  #serial reader/writer for data collection
 sudo echo "echo "Loading starting programs"" >> /home/pi/.bashrc
-sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/UpdateGit.bash" >> /home/pi/.bashrc
+sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/UpdateGit.bash" >> /home/pi/.bashrc #places updategit to start at start up
 
 echo "Which Project do you want to run?"
 select pr in "RobotArm" "Pick2Light" "Standard"; do
@@ -60,21 +61,21 @@ select pr in "RobotArm" "Pick2Light" "Standard"; do
         break;; 
       Pick2Light) 
         
-	echo "Pick the Light"
+	echo "Pick the Light" #creates token folder and data folder for use with slackclient and data collection
 	cd /home/pi/P2LightData
 	cd /home/pi/P2LightData/Tokens
 	cd /home/pi/P2LightData/Data
 
-	sudo echo "sudo bash /home/pi/Scripts/Pick2Light/RaspberryPiCode/Pick2Light.bash" >> /home/pi/.bashrc
-        sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/SlackBot/Slack.bash" >> /home/pi/.bashrc
+	sudo echo "sudo bash /home/pi/Scripts/Pick2Light/RaspberryPiCode/Pick2Light.bash" >> /home/pi/.bashrc #allows Pick2Light to run at start u
+        sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/SlackBot/Slack.bash" >> /home/pi/.bashrc #allows for Slack Bot to run at start up
 
 	break;;
       Standard) 
 	echo "Get the Scrap"
-	sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/SlackBot/Slack.bash" >> /home/pi/.bashrc
+	sudo echo "sudo bash /home/pi/Scripts/RaspberryPIScripts/SlackBot/Slack.bash" >> /home/pi/.bashrc #slackbot start up bash
 	break;;
     esac
 done
 
 echo "Finished"
-sudo reboot
+sudo reboot #reboots to finalize installs
