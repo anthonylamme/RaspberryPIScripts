763c16f505438a25c0ffff1049e890a4700226bc
anthonylamme <anthonylamme@gmail.com>
2017-07-31 09:14:23 -0500

    Update BotOps.py

--- SlackBot/BotOps.py
+++ SlackBot/BotOps.py
@@ -1,27 +1,40 @@
-import os as os
-import time as time
 """
-sleep 30s; shutdown -h now
+Custom Library to help with command control of python slack bot
+
 """
-path="wow"
+import os as os #used for OS related tasks
+import time as time #used for time control
+
+
+"""
+Shuts down system after seconds. Seconds can be a decimal but was designed for integer values
+"""
 def ShutDownOp(seconds):
     
     time.sleep(seconds)
-    os.system("sudo shutdown now -h")
-    
+    os.system("sudo shutdown now -h")#sends command to shut system down
+"""
+Restarts system after a number of seconds. Can be Float
+"""
 def RestartOp(seconds):
     
     time.sleep(seconds)
     os.system("sudo shutdown -r -t 1")
-
+"""
+Cancels system command to shutdown/restart
+"""
 def CancelOp():
 
     os.system("sudo shutdown -c")
-    
+"""
+Updates Programs running on RPi
+"""
 def UpdateGit():
 
     os.system("sudo bash /home/pi/Scripts/RaspberryPIScripts/UpdateGit.bash")
-    
+"""
+Will update Slack Bot. Still needs work
+"""
 def UpdateBot():
 
     os.system("sudo bash /home/pi/Scripts/home/pi/Scripts/RaspberryPIScripts/replaceMeBot.bash")
