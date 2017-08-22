03ffd174fb3221310955f5130a0baf4d5246d7e8
anthonylamme <anthonylamme@gmail.com>
2017-08-22 15:10:43 -0500

    Update SlackBot.py

--- SlackBot/SlackBot.py
+++ SlackBot/SlackBot.py
@@ -38,7 +38,7 @@ slack_client =SlackClient(Token)
 user_list = slack_client.api_call("users.list") #files self on user list. if there is a error with this line it is the Token not the code
 for user in user_list.get('members'):
     print user.get('name')
-    if user.get('name') == myName: #makes sure it has its name as a id
+    if user.get('name') == 'printscannerpi1': #makes sure it has its name as a id
         slack_user_id = user.get('id') #retrieves slack encoded id
         print user.get('id')
         break
