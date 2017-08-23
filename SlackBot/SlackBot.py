b913df18decaed90831deea5866bb8d2439d1fe0
anthonylamme <anthonylamme@gmail.com>
2017-08-23 10:03:04 -0500

    Update SlackBot.py

--- SlackBot/SlackBot.py
+++ SlackBot/SlackBot.py
@@ -41,9 +41,6 @@ for user in user_list.get('members'):
         slack_user_id = user.get('id') #retrieves slack encoded id
         print user.get('id')
         break
-    else:
-        print user.get('name').len
-        print Number.len
 if slack_client.rtm_connect():#connects to slack client
     print "Connected" #sanity check
     slack_client.rtm_send_message('allpis',"%s here"%Number) #will notify all channel that its online
