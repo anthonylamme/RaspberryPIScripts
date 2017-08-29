3cb52a320345797bdab37db5fbad66e109356386
anthonylamme <anthonylamme@gmail.com>
2017-08-29 17:36:17 -0500

    Update SlackBot.py

--- SlackBot/SlackBot.py
+++ SlackBot/SlackBot.py
@@ -20,9 +20,9 @@ TokenAdd=MainFolder+'Tokens/Token.txt'
 
 
 file_obj=open(TokenAdd,'r')
-Number=file_obj.readline().rstrip().strip(' ')
-myName=file_obj.readline().rstrip().strip(' ')
-Token=file_obj.readline().rstrip().strip(' ')
+Number=file_obj.readline().rstrip().strip(' ').strip()
+myName=file_obj.readline().rstrip().strip(' ').strip()
+Token=file_obj.readline().rstrip().strip(' ').strip()
 file_obj.close()
 
 csv_path=MainFolder+'ItemList.csv'
